40a88bda88ce97064ae2452ee4960f2629081e00
tusderechos <hidrosoluciones61@gmail.com>
2025-04-25 16:04:03 -0600

    Create Santos_Daniel_Formulas.java

---
+++ Santos_Daniel_Formulas.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab_Programacion1;
+
+/**
+ *
+ * @author Hp
+ */
+public class Santos_Daniel_Formulas {
+    
+}
