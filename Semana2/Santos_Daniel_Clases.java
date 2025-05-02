a4b1b5527145109df33f4af7eeb87b6e2b13b429
tusderechos <hidrosoluciones61@gmail.com>
2025-05-02 13:21:40 -0600

    asdf

--- Semana2/Santos_Daniel_Clases.java
+++ Semana2/Santos_Daniel_Clases.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Lab_Programacion1;
+package Lab_Programacion1.Semana2;
 
 /**
  *
  * @author Hp
  */
-public class Santos_Daniel_Formulas {
+public class Santos_Daniel_Clases {
     
 }
